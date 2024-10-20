3b38c67e16d1471e56906f05e4c02ec6f2e1df02
momo <moued7584@gmail.com>
2024-10-20 19:24:23 -0400

    'lab5'

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,19 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import {BehaviorSubject, Observable} from "rxjs";
 import {Employee} from "../model/employee";
+import {
+  Firestore,
+  collection,
+  collectionData,
+  addDoc,
+  doc,
+  setDoc
+} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private firestore: Firestore = inject(Firestore);
   employees$: BehaviorSubject<readonly Employee[]> = new BehaviorSubject<readonly Employee[]>([]);
 
   get $(): Observable<readonly Employee[]> {
     return this.employees$;
   }
 
-  addEmployee(employee: Employee) {
-    this.employees$.next([...this.employees$.getValue(), employee]);
-    return true;
+  async addEmployee(employee: Employee): Promise<void> {
+    try {
+
+      const employeesCollection = collection(this.firestore, 'employees');
+
+      await addDoc(employeesCollection, { ...employee });
+
+      this.employees$.next([...this.employees$.getValue(), employee]);
+      console.log('Employee added successfully!');
+    } catch (error) {
+      console.error('Error adding employee: ', error);
+    }
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyB-59KL2F9-ilN2_BitFxM1vtfoso3fKJg",
+    authDomain: "lab5seg3102-4f7e2.firebaseapp.com",
+    projectId: "lab5seg3102-4f7e2",
+    storageBucket: "lab5seg3102-4f7e2.appspot.com",
+    messagingSenderId: "256689741670",
+    appId: "1:256689741670:web:b9d45d77346d4ce319b2a7",
+    measurementId: "G-R2V0XGW408"
+  }
+};
